7747a7e203095ecfe92610a80cec8d2d2bbce0c7
Tanawut Wongboot <tanawut.w@outlook.com>
2023-11-27 16:16:15 +0700

    feat(ex-3): refactor ex-1 function from declaration to arrow function

--- ex-3.js
+++ ex-3.js
@@ -1,1 +1,37 @@
 // Start coding here
+// function add(a, b) {
+//     const result = a + b;
+
+//     return result;
+// }
+let add = (a, b) => a + b;
+let testAdd = add(10, 5);
+console.log("🚀 ~ file: ex-2.js:9 ~ testAdd:", testAdd)
+
+
+// function subtract(a, b) {
+//     const result = a - b;
+
+//     return result;
+// }
+let subtract = (a, b) => a - b;
+let testSubtract = subtract(10, 5);
+console.log("🚀 ~ file: ex-2.js:19 ~ testSubtract:", testSubtract)
+
+// function multiply(a, b) {
+//     const result = a * b;
+
+//     return result;
+// }
+let multiply = (a, b) => a * b;
+let testMultiply = multiply(10, 5);
+console.log("🚀 ~ file: ex-2.js:28 ~ testMultiply:", testMultiply)
+
+// function divide(a, b) {
+//     const result = a / b;
+
+//     return result;
+// }
+let divide = (a, b) => a / b;
+let testDivide = divide(10, 5);
+console.log("🚀 ~ file: ex-2.js:37 ~ testDivide:", testDivide)
